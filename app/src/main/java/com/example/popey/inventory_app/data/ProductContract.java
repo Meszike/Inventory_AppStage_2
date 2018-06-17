da25ee942b17f2cb454f521cc29154671a4faeed
Meszike <poborik@gmail.com>
2018-06-17 15:12:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/popey/inventory_app/data/ProductContract.java
@@ -0,0 +1,87 @@
+package com.example.popey.inventory_app.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class ProductContract  {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private ProductContract() {
+    }
+
+    public static final String CONTENT_AUTHORITY = "com.example.popey.inventory_app";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_INVENTORYAPP = "product";
+
+    /**
+     * Inner class that defines constant values for the product database table.
+     * Each entry in the table represents a single product.
+     */
+    public static final class ProductEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_INVENTORYAPP);
+
+        public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORYAPP;
+
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORYAPP;
+
+        /** Name of database table for products */
+        public final static String TABLE_NAME = "product";
+
+        /**
+         * Unique ID number for the product (only for use in the database table).
+         *
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+        /**
+         * Name of the product.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_PRODUCT_NAME ="name";
+
+        /**
+         * Price of the product.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PRODUCT_PRICE ="price";
+
+        /**
+         * Quantity of the product.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PRODUCT_QUANTITY ="quantity";
+
+        /**
+         * Photo of the product.
+         *
+         * Type: BLOB
+         */
+        public final static String COLUMN_PRODUCT_IMAGE ="image";
+
+        /**
+         * Supplier of the product.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_PRODUCT_SUPPLIER_NAME ="supplier";
+
+        /**
+         * Supplier's Phone.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_PRODUCT_SUPPLIER_PHONE ="phone";
+
+    }
+
+
+}
